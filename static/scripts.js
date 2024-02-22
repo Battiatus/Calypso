d09dc3b1e0456f719890b028e355be1d37634cf1
Battiatus <50103241+Battiatus@users.noreply.github.com>
2024-02-22 13:02:10 +0100

    Add file upload functionality and loading indicator

--- static/scripts.js
+++ static/scripts.js
@@ -1,12 +1,13 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-    var form = document.querySelector('form');
     var dropZone = document.getElementById("drop_zone");
-    var spinner = document.getElementById("spinner"); // Assurez-vous d'avoir un élément avec l'id "spinner"
+    var uploadForm = document.getElementById("upload_form");
+    var loadingIndicator = document.getElementById("loading_indicator");
 
     dropZone.addEventListener("dragover", function(e) {
         e.stopPropagation();
         e.preventDefault();
-        e.dataTransfer.dropEffect = "copy"; // Affiche l'icône de copie
+        e.dataTransfer.dropEffect = "copy";
     });
 
     dropZone.addEventListener("drop", function(e) {
@@ -15,12 +16,26 @@ document.addEventListener("DOMContentLoaded", function() {
         var files = e.dataTransfer.files;
         if (files.length) {
             document.getElementById("file").files = files;
-            spinner.style.display = "block"; // Affiche le spinner
-            form.submit(); // Soumet le formulaire
+            uploadForm.style.display = "block"; // Afficher le formulaire d'upload
         }
     });
 
-    form.onsubmit = function() {
-        spinner.style.display = "block"; // Affiche le spinner lors de la soumission du formulaire
-    };
+    uploadForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        var formData = new FormData(uploadForm);
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/");
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                // Conversion réussie, masquer l'indicateur de chargement
+                loadingIndicator.style.display = "none";
+            } else {
+                // Gérer les erreurs
+                alert("Une erreur s'est produite lors de la conversion.");
+            }
+        };
+        // Afficher l'indicateur de chargement avant d'envoyer la requête
+        loadingIndicator.style.display = "block";
+        xhr.send(formData);
+    });
 });
