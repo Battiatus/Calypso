7c95352eee408217e9ff70456591f8fb2b7c957c
Battiatus <50103241+Battiatus@users.noreply.github.com>
2024-02-22 11:38:25 +0100

    Add file upload functionality to Flask server. first step refacto

--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,3 @@
-document.getElementById('drop_zone').addEventListener('click', function() {
-    document.getElementById('file_input').click();
-});
-
 document.getElementById('drop_zone').addEventListener('dragover', function(event) {
     event.stopPropagation();
     event.preventDefault();
@@ -12,10 +8,23 @@ document.getElementById('drop_zone').addEventListener('drop', function(event) {
     event.stopPropagation();
     event.preventDefault();
     const files = event.dataTransfer.files;
-    console.log(files);
-    // Ici, ajoutez la logique pour envoyer les fichiers au serveur
-});
+    const formData = new FormData();
+    formData.append('file', files[0]);  // Exemple avec un seul fichier
+    formData.append('format', 'json');  // Exemple : spécifiez le format souhaité ici
 
-document.getElementById('upload_btn').addEventListener('click', function() {
-    // Logique pour télécharger le résultat de la conversion
+    fetch('/upload', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = files[0].name + '.json';  // Assurez-vous que cela correspond au format de fichier retourné
+        document.body.appendChild(a); // Nécessaire pour Firefox
+        a.click();
+        a.remove();
+    })
+    .catch(error => console.error('Error:', error));
 });
